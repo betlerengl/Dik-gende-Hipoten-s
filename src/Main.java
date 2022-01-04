8b6fccd22fbbeb6075ea1fb2f14d82577d70e723
betlerengl <haticebetul2000@hotmail.com>
2022-01-04 20:15:19 +0300

    second commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+       // Değişkenlerimizi oluşturalım.
+        int a , b;
+        double c;
+
+       // Kullanıcıdan verilerimizi alalım.
+       Scanner girdi = new Scanner(System.in);
+       System.out.println("1.Kenarı Giriniz :  ");
+       a  = girdi.nextInt();
+       System.out.println("2.Kenarı Giriniz :  ");
+       b = girdi.nextInt();
+
+
+       c = Math.sqrt((a*a)+ (b*b));
+       System.out.println("Hipotenüs: " + c );
+    }
+}
